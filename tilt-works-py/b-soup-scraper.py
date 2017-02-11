cb3320c072d849723f77d32be925acdd690d9e38
Harsh Pandey <harshvardhanpandey@hotmail.com>
2017-02-11 15:27:36 -0500

    Update b-soup-scraper.py

--- tilt-works-py/b-soup-scraper.py
+++ tilt-works-py/b-soup-scraper.py
@@ -3,20 +3,7 @@ from urllib2 import urlopen
 import requests as rq
 import re
 
-urls = ["http://www.merckmillipore.com/INTL/en/product/HBSS-with-Phenol-Red,EMD_BIO-L"]
-# "http://www.emdmillipore.com/US/en/product/Alcian-Blue%2CEMD_CHEM-109",
-# "http://www.emdmillipore.com/US/en/product/Aniline-Blue-W.S.%2CEMD_CHEM-128",
-# "http://www.emdmillipore.com/US/en/product/Biebrich-Scarlette%2CEMD_CHEM-148",
-# "http://www.emdmillipore.com/US/en/product/Brilliant-Green%2CEMD_CHEM-162",
-# "http://www.emdmillipore.com/US/en/product/Cresyl-Blue-Brilliant%2CEMD_CHEM-163",
-# "http://www.emdmillipore.com/US/en/product/Carmine-Aluminum-Lake%2CEMD_CHEM-168",
-# "http://www.emdmillipore.com/US/en/product/Congo-Red%2CEMD_CHEM-186",
-# "http://www.emdmillipore.com/US/en/product/Cresyl-Violet%2CEMD_CHEM-190",
-# "http://www.emdmillipore.com/US/en/product/Crystal-Violet%2CEMD_CHEM-192",
-# "http://www.emdmillipore.com/US/en/product/Eosin-Y-Powder%2CEMD_CHEM-200",
-# "http://www.emdmillipore.com/US/en/product/Fast-Green%2CEMD_CHEM-210",
-# "http://www.emdmillipore.com/US/en/product/Fuchsin-Acid%2CEMD_CHEM-218",
-# "http://www.emdmillipore.com/US/en/product/Fuchsin-Basic%2CEMD_CHEM-220"]
+urls = ["http://www.example.com", 'www.example1.com']
 for page in urls:
     sauce = urlopen(page).read()
     soup = bsoup(sauce, 'lxml')
